d0a13b20f1419a15d7160af9d03172efc82c7677
Shreyash Reshu <shreyashreshu6@gmail.com>
2026-02-01 20:46:04 +0530

    fixed userRepo for RBAC Bug

--- src/main/java/com/milmgt/repository/UserRepository.java
+++ src/main/java/com/milmgt/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package com.milmgt.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.milmgt.entity.User;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.base WHERE u.username = :username")
+    Optional<User> findByUsername(@Param("username") String username);
 }
